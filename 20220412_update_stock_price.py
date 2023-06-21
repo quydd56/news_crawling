f3e020fcfefe816bd891db07f8049d8b74e6b701
quydd56 <53222511+quydd56@users.noreply.github.com>
2023-06-21 22:48:43 +0700

    Update 20220412_update_stock_price.py

--- 20220412_update_stock_price.py
+++ 20220412_update_stock_price.py
@@ -10,7 +10,7 @@ import time
 server = 'DESKTOP-Q21C3LU\SQLEXPRESS'
 database = 'stockdata_qdd'
 username = 'sa'
-password = 'abc@1234'
+password = 
 cnxn = pyodbc.connect(
     'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 
@@ -23,8 +23,8 @@ cursor.close()
 
 # function send telegram message
 def telegram_bot_sendtext(bot_message):
-    bot_token = '5138369875:AAFnh2Fe4buI4RqKzQJJsQ01GL7fizWBU9M'
-    bot_chatID = '1962268782'
+    bot_token = 
+    bot_chatID =
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
     return response.json()

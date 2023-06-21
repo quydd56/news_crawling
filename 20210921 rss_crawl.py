866e8955a271e6dff4a0a59f4124004591569f2c
quydd56 <53222511+quydd56@users.noreply.github.com>
2023-06-21 23:47:41 +0700

    Update 20210921 rss_crawl.py

--- 20210921 rss_crawl.py
+++ 20210921 rss_crawl.py
@@ -141,7 +141,7 @@ start_time = time.time()
 server = 'DESKTOP-Q21C3LU\SQLEXPRESS'
 database = 'stockdata_qdd'
 username = 'sa'
-password = 'abc@1234'
+password = 
 cnxn = pyodbc.connect(
     'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 cursor = cnxn.cursor()

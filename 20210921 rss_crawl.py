8b1cc15a53df1d09ca1c44608589132b737de898
quydd56 <53222511+quydd56@users.noreply.github.com>
2023-06-21 22:47:12 +0700

    Update 20210921 rss_crawl.py

--- 20210921 rss_crawl.py
+++ 20210921 rss_crawl.py
@@ -284,8 +284,8 @@ title = cursor.execute("SELECT title "
 
 # Insert all_link into SQL Server:
 def telegram_bot_sendtext(bot_message):
-    bot_token = '2049849672:AAFD-CbFPdZXeCbsLttSWZ07Uj95Dxer1zw'
-    bot_chatID = '1962268782'
+    bot_token = 
+    bot_chatID = 
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
     return response.json()
